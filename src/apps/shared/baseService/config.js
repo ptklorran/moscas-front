e37d8a7f9af267aeea2c2e80985e17671135c283
Patrick <ptklorran@gmail.com>
2022-11-18 00:19:36 -0300

    up

--- src/apps/shared/baseService/config.js
+++ src/apps/shared/baseService/config.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const http = axios.create({
   // baseURL: process.env.VUE_APP_URL_API,
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "http://200.129.165.106/bases/",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${
